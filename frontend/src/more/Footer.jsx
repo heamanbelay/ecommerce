9842d75a42742035c1f08b4d2c93a604376ee904
heamanbelay <heamanbelay@gmail.com>
2022-11-03 14:28:32 +0300

    Add existingjjj

--- frontend/src/more/Footer.jsx
+++ frontend/src/more/Footer.jsx
@@ -67,7 +67,7 @@ const Footer = () => {
             <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
             <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
           </svg>
-          <strong>Time:</strong>
+          <strong>Working Hours:</strong>
           <h6>9:00 am - 6:00 pm (monday - saturday)</h6>
         </div>
         <br></br>
